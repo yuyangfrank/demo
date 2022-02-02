43051035bf1b6c2fc9a10925e957bfec362cd468
Yang <yuyangfrank@gmail.com>
2022-02-02 18:51:21 +0800

    Add Error logging and handling

--- src/main/java/com/example/demo/service/CsvFileService.java
+++ src/main/java/com/example/demo/service/CsvFileService.java
@@ -4,7 +4,6 @@ import com.example.demo.repository.User;
 import com.example.demo.repository.UserRepo;
 import com.opencsv.CSVParser;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,6 +14,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Service
 @AllArgsConstructor
@@ -23,24 +23,55 @@ public class CsvFileService {
     @Autowired
     private final UserRepo repo = null;
 
-    public Boolean loadUserData(MultipartFile file) throws IOException {
+    public Boolean loadUserData(MultipartFile file) throws RuntimeException {
+        AtomicBoolean fileErr = new AtomicBoolean(false);
         ArrayList<User> users = new ArrayList<>();
         CSVParser parser = new CSVParser();
 
-        new BufferedReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8))
-                .lines()
-                .skip(1)
-                .forEach(line -> {
-                    String[] params = new String[0];
-                    try {
-                        params = parser.parseLine(line);
-                    } catch (IOException e) {
-                        System.out.println("Ex Message: " + e.getMessage());
-                        System.out.println("Ex Cause: " + e.getCause());
-                        e.printStackTrace();
-                    }
-                    users.add(new User(params[0], Double.parseDouble(params[1])));
-                });
+        try {
+            new BufferedReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8))
+                    .lines()
+                    .skip(1)
+                    .forEach(line -> {
+
+                        if(line.equals("")) return;
+
+                        String[] params = new String[0];
+                        try {
+                            params = parser.parseLine(line);
+                        } catch (IOException e) {
+                            System.out.println("=======>"+ e.getCause());
+                            System.out.println("Line: " + line);
+//                            e.printStackTrace();
+                            fileErr.set(true);
+//                            throw new RuntimeException(e);
+                        }
+
+                        if (params.length != 2) {
+                            System.out.println("=====> Invliad column count.");
+                            System.out.println("Line: " + line);
+                            fileErr.set(true);
+//                            throw new RuntimeException("Invliad column count. Line: " + line);
+                        }
+                        try {
+                            users.add(new User(params[0], Double.parseDouble(params[1])));
+                        } catch (Exception ex) {
+                            System.out.println("=====> Invliad number.");
+                            System.out.println("Line: " + line);
+                            fileErr.set(true);
+                        }
+                    });
+
+        } catch (IOException e) {
+//            e.printStackTrace();
+            System.out.println("file error when getInputStream()");
+            fileErr.set(true);
+//            throw new RuntimeException(e);
+        }
+
+        if(fileErr.get()) {
+            throw new RuntimeException("File processing failed.");
+        }
 
         saveUsers(users);
 
